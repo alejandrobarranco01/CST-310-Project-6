682b84f457515ee2cef4faee9c509e82340d9f2a
Alejandro Barranco-Leyte <alejandrobarranco01@gmail.com>
2024-03-03 14:20:01 -0700

    Added comments

--- Project-6.cpp
+++ Project-6.cpp
@@ -12,41 +12,41 @@
 #include <limits>
 #include <GL/freeglut_std.h>
 
+// Function prototypes
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
 void do_movement();
 void moveLighting(GLFWwindow *window);
 int getUserInput();
 
-// settings
+// Window settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-int shininess = 256;
+int shininess = 256; // Initial shininess value
 
-// Camera
+// Camera settings
 glm::vec3 cameraPos = glm::vec3(-1.12255f, -0.00404159f, 2.48081f);
-glm::vec3 cameraFront = glm::vec3(0.0f, 2.0f, 0.0f); // rotation
+glm::vec3 cameraFront = glm::vec3(0.0f, 2.0f, 0.0f); 
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
-// For camera rotation
-GLfloat yaw = -64.5f; // Yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right
-GLfloat pitch = 3.5f; // Pitch is initialized to 5.0 degrees downward
+// Camera rotation parameters
+GLfloat yaw = -64.5f; 
+GLfloat pitch = 3.5f;
 
-// buffer for keyboard input
+// Keyboard input buffer
 bool keys[1024];
 
-// timing
+// Timing variables
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
-// lighting
+// Light position
 glm::vec3 lightPos(0.5f, 0.0f, 2.0f);
 
 int main()
 {
-    // glfw: initialize and configure
-    // ------------------------------
+    // Initialize GLFW
     if (!glfwInit())
     {
         std::cout << "Failed to initialize GLFW" << std::endl;
@@ -60,8 +60,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    // glfw window creation
-    // --------------------
+    // Create GLFW window
     GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Project 6", NULL, NULL);
     if (window == NULL)
     {
@@ -73,25 +72,21 @@ int main()
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glfwSetKeyCallback(window, key_callback);
 
-    // GLEW: initialize
-    // ----------------
+    // Initialize GLEW
     if (glewInit() != GLEW_OK)
     {
         std::cout << "Failed to initialize GLEW" << std::endl;
         return -1;
     }
 
-    // configure global opengl state
-    // -----------------------------
+    // Enable depth testing
     glEnable(GL_DEPTH_TEST);
 
-    // build and compile our shader zprogram
-    // ------------------------------------
+    // Create shaders
     Shader lightingShader("Light.vs", "Light.frag");
     Shader lightCubeShader("Cube-Light.vs", "Cube-Light.frag");
 
-    // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
+    // Vertex data for cube
     float vertices[] = {
         -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
         0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
@@ -134,39 +129,39 @@ int main()
         0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
         -0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
         -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f};
-    // first, configure the cube's VAO (and VBO)
+
+    // Set up vertex data and buffers
     unsigned int VBO, cubeVAO;
     glGenVertexArrays(1, &cubeVAO);
     glGenBuffers(1, &VBO);
 
+    // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s)
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     glBindVertexArray(cubeVAO);
 
-    // position attribute
+     // Set vertex attributes
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
-    // normal attribute
+    // Set normal attributes
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    // second, configure the light's VAO (VBO stays the same; the vertices are the same for the light object which is also a 3D cube)
+     // Light cube VAO (same as cube VAO)
     unsigned int lightCubeVAO;
     glGenVertexArrays(1, &lightCubeVAO);
     glBindVertexArray(lightCubeVAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    // note that we update the lamp's position attribute's stride to reflect the updated buffer data
+    
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
 
-    // render loop
-    // -----------
+    // Render loop
     while (!glfwWindowShouldClose(window))
     {
-        // per-frame time logic
-        // --------------------
+        // Time logic
         float currentFrame = static_cast<float>(glfwGetTime());
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
@@ -174,13 +169,13 @@ int main()
         do_movement();
         moveLighting(window);
 
-        // render
-        // ------
+        // Rendering
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // be sure to activate shader when setting uniforms/drawing objects
+        // Use lighting shader
         lightingShader.use();
+        // Set shader uniforms
         lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
         lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
         lightingShader.setVec3("lightPos", lightPos);
@@ -189,11 +184,12 @@ int main()
         if (shininessLocation == -1)
         {
             std::cerr << "Failed to locate the shininess uniform in the shader program!" << std::endl;
-            // Handle the error appropriately
         }
+
         // Set the default value of shininess
         glUniform1i(shininessLocation, shininess);
 
+        // Check if the user wants to change the value of shininess
         if (glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS)
         {
             int userInput = getUserInput();
@@ -202,64 +198,64 @@ int main()
             shininess = userInput;
         }
 
-        // view/projection transformations
+        // View/Projection transformations
         glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
         glm::mat4 projection = glm::perspective(45.0f, (GLfloat)SCR_WIDTH / (GLfloat)SCR_HEIGHT, 0.1f, 100.0f);
         lightingShader.setMat4("projection", projection);
         lightingShader.setMat4("view", view);
 
-        // world transformation
+        // World transformation
         glm::mat4 model = glm::mat4(1.0f);
         lightingShader.setMat4("model", model);
 
-        // render the cube
+        // Render the cube
         glBindVertexArray(cubeVAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
-        // also draw the lamp object
+        // Use light cube shader
         lightCubeShader.use();
+        // Set shader uniforms
         lightCubeShader.setMat4("projection", projection);
         lightCubeShader.setMat4("view", view);
         model = glm::mat4(1.0f);
         model = glm::translate(model, lightPos);
-        model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
+        model = glm::scale(model, glm::vec3(0.2f)); 
         lightCubeShader.setMat4("model", model);
 
+        // Draw light cube
         glBindVertexArray(lightCubeVAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
+        // Swap buffers
         glfwSwapBuffers(window);
+        // Poll for and process events
         glfwPollEvents();
     }
 
-    // optional: de-allocate all resources once they've outlived their purpose:
-    // ------------------------------------------------------------------------
+    // De-allocate all resources once they've outlived their purpose
     glDeleteVertexArrays(1, &cubeVAO);
     glDeleteVertexArrays(1, &lightCubeVAO);
     glDeleteBuffers(1, &VBO);
 
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
+    // Terminate GLFW, clearing all previously allocated GLFW resources 
     glfwTerminate();
     return 0;
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
+// Framebuffer resize callback function
 void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
-    // make sure the viewport matches the new window dimensions; note that width and
-    // height will be significantly larger than specified on retina displays.
+    // Resize viewport
     glViewport(0, 0, width, height);
 }
 
+// Keyboard callback function
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
 {
+    // Handle escape key to close window
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
-
+     // Store keyboard state
     if (key >= 0 && key < 1024)
     {
         if (action == GLFW_PRESS)
@@ -269,28 +265,29 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
     }
 }
 
+// Process keyboard input for camera movement
 void do_movement()
 {
     // Camera controls
     GLfloat rotationSpeed = 1.5f; // camera rotation speed
     GLfloat cameraSpeed = 0.025f; // camera movement speed
 
-    // Press 1 to reset the camera
+    // Reset camera position
     if (keys[GLFW_KEY_1])
     {
         cameraPos = glm::vec3(-1.12255f, -0.00404159f, 2.48081f);
-        cameraFront = glm::vec3(0.0f, 0.0f, -1.0f); // rotation
+        cameraFront = glm::vec3(0.0f, 0.0f, -1.0f); 
         cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-        yaw = -64.5f; // reset yaw to reset camera rotation
-        pitch = 3.5f; // Pitch is initialized to 0.0 degrees
+        yaw = -64.5f; 
+        pitch = 3.5f; 
         lightPos = glm::vec3(0.5f, 0.0f, 2.0f);
     }
 
-    if (keys[GLFW_KEY_W])
+    if (keys[GLFW_KEY_W]) // Move camera forward or backward using W and S
         cameraPos += cameraSpeed * cameraFront;
     if (keys[GLFW_KEY_S])
         cameraPos -= cameraSpeed * cameraFront;
-    if (keys[GLFW_KEY_A])
+    if (keys[GLFW_KEY_A]) // Move camera left or right using A and D
         cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
     if (keys[GLFW_KEY_D])
         cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
@@ -327,9 +324,10 @@ void do_movement()
     cameraFront = glm::normalize(front);
 }
 
+// Process keyboard input for moving the light source
 void moveLighting(GLFWwindow *window)
 {
-    float lightSpeed = 0.5f; // Adjust as needed
+    float lightSpeed = 0.5f; 
 
     if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS)
         lightPos.y += lightSpeed * deltaTime;
@@ -345,6 +343,7 @@ void moveLighting(GLFWwindow *window)
         lightPos.z += lightSpeed * deltaTime;
 }
 
+// Get user input for shininess value
 int getUserInput()
 {
     int userInput;
